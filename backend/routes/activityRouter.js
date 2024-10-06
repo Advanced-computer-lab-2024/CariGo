ca1ab35ed031949536aaafb7e54be23d8eac5ecf
mennawahba <melgazar2003@gmail.com>
2024-10-06 16:51:44 +0300

    nargooo 3adam ezalet elautherization mn el code

--- backend/routes/activityRouter.js
+++ backend/routes/activityRouter.js
@@ -15,7 +15,7 @@ router.get('/getOne/:id',authController.restrictTo("Advertiser"), activityContro
 
 
 
-router.post('/createActivity', activityController.createActivity);
+router.post('/createActivity',[authController.protect ,authController.restrictTo("Advertiser")], activityController.createActivity);
 
 // router.post('/createActivity',[authController.protect ,authController.restrictTo("Advertiser")], activityController.createActivity);
 router.patch('/updateActivity/:id',authController.restrictTo("Advertiser"),activityController.updateActivity);
