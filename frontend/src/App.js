de2cf2fb82f91b995b0ed1bd908f03dc16d61e35
0rahma0 <03rahma.ahmed@gmail.com>
2024-10-07 19:35:48 +0300

    2y 7aga

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,22 @@ import "./styles/App.css";
 import React from "react";
 import AdvertiserProfile from "./AdvertiserProfile"; // Adjust the path based on your structure
 //import './styles/App.css';
-import './styles/index.css';
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
-import UserViewActivities from './pages/UserViewActivities';
-import Home from './pages/Home';
-import LoginPage from './pages/login';
-import { jwtDecode } from 'jwt-decode';
+import "./styles/index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import UserViewActivities from "./pages/UserViewActivities";
+import UserViewItineraries from "./pages/UserViewItineraries";
+import Home from "./pages/Home";
+import LoginPage from "./pages/login";
+import SignUp from "./pages/Signup";
+import { jwtDecode } from "jwt-decode";
 import NavBar from "./components/NavBar";
-import CreateActivityForm from "./pages/CreateActivityForm";
-import TouristProfile from "./pages/TouristProfile";
-import UpdateActivityForm from "./components/UpdateActivityForm";
-
+import ItineraryUpdate from "./components/ItineraryUpdate";
+import ItineraryDetails from "./pages/ItineraryDetails";
+import CreateItineraryForm from "./components/CreateItineraryForm";
+import AllItineraries from "./pages/AllItineraries";
+import UserItinDetails from './pages/UserItinDetails'
 function App() {
-  const token = localStorage.getItem('jwt');
+  const token = localStorage.getItem("jwt");
 
   // Decode the token safely
   let userId;
@@ -23,22 +26,34 @@ function App() {
       const decoded = jwtDecode(token);
       userId = decoded.id; // Assuming the id is in the decoded token
     } catch (error) {
-      console.error('Failed to decode token:', error);
+      console.error("Failed to decode token:", error);
     }
   }
   return (
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Default route */}
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/activities" element={<UserViewActivities />} /> 
-        <Route path="/createActivity" element={<CreateActivityForm userId={localStorage.getItem("id")} />} /> 
-        <Route path="/advertiser" element={<AdvertiserProfile userId={localStorage.getItem("id")} />} />
-        <Route path="/tourist" element={<TouristProfile userId={localStorage.getItem("id")} />} />
-        {/* <Route path="/activities/:id" element={<ActivityDetail/>} /> */}
-        <Route path="/activities/update/:id" element={<UpdateActivityForm/>} />
-        {/* Add more routes as needed */}
-      </Routes>
-    
+    <Routes>
+      <Route path="/" element={<Home />} /> {/* Default route */}
+      <Route path="tour_guide/itineraries" element={<UserViewItineraries />} />
+      <Route path="tour_guide/itineraries/:id" element={<ItineraryDetails />} />
+      <Route path="tour_guide/itineraries/new" element={<CreateItineraryForm />} />
+      <Route path="tour_guid/itineraries/:id/edit" element={<ItineraryUpdate />} />
+      <Route path="/user_itineraries" element={<AllItineraries />} />
+      <Route path="/user_itineraries/:id" element={<UserItinDetails />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="/activities" element={<UserViewActivities />} />
+      <Route
+        path="/advertiser"
+        element={<AdvertiserProfile userId={localStorage.getItem("id")} />}
+      />
+      {/* <Route path="/activities/:id" element={<ActivityDetail/>} /> */}
+      <Route path="/activities/update/:id" element={<updateActivityForm />} />
+      <Route path="/" element={<Home />} /> {/* Default route */}
+      <Route path="/activities" element={<UserViewActivities />} /> 
+      <Route path="/createActivity" element={<CreateActivityForm userId={localStorage.getItem("id")} />} /> 
+      <Route path="/advertiser" element={<AdvertiserProfile userId={localStorage.getItem("id")} />} />
+      <Route path="/tourist" element={<TouristProfile userId={localStorage.getItem("id")} />} />
+      {/* Add more routes as needed */}
+    </Routes>
   );
 }
 
--- frontend/src/pages/Placeholder.js
+++ frontend/src/pages/Placeholder.js
@@ -1,13 +1,13 @@
   // return (
   //     <Routes>
-  //       <Route path="/" element={<Home />} /> {/* Default route */}
-  //       <Route path="/login" element={<LoginPage />} />
-  //       <Route path="/activities" element={<UserViewActivities />} /> 
-  //       <Route path="/createActivity" element={<CreateActivityForm userId={localStorage.getItem("id")} />} /> 
-  //       <Route path="/advertiser" element={<AdvertiserProfile userId={localStorage.getItem("id")} />} />
-  //       <Route path="/tourist" element={<TouristProfile userId={localStorage.getItem("id")} />} />
-  //       {/* <Route path="/activities/:id" element={<ActivityDetail/>} /> */}
-  //       <Route path="/activities/update/:id" element={<UpdateActivityForm/>} />
+        // <Route path="/" element={<Home />} /> {/* Default route */}
+        // <Route path="/login" element={<LoginPage />} />
+        // <Route path="/activities" element={<UserViewActivities />} /> 
+        // <Route path="/createActivity" element={<CreateActivityForm userId={localStorage.getItem("id")} />} /> 
+        // <Route path="/advertiser" element={<AdvertiserProfile userId={localStorage.getItem("id")} />} />
+        // <Route path="/tourist" element={<TouristProfile userId={localStorage.getItem("id")} />} />
+        // {/* <Route path="/activities/:id" element={<ActivityDetail/>} /> */}
+        // <Route path="/activities/update/:id" element={<UpdateActivityForm/>} />
   //       {/* Add more routes as needed */}
   //     </Routes>
     
