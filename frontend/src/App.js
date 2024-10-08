dd2017746a5e2b3b98725e464eb573d8020d20ab
mohamed-abdulbaki22 <mohamedabdulbaki30@gmail.com>
2024-10-08 05:03:39 +0300

    ya rab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,8 @@ import  TouristItineraries from "./pages/Tourist/TouristItineraries";
 import  GuestItineraries from "./pages/Guest/GuestItineraries";
 import  GuestViewVintage from "./pages/Guest/GuestPlaces";
 import  TouristVintage from "./pages/Tourist/TouristPlaces";
-
+import CreateVintageForm from "./pages/CreateVintageForm.js";
+import SellerProfile from "./pages/SellerProfile.js";
 
 import './styles/index.css';
 import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
@@ -91,6 +92,10 @@ function App() {
         <Route path="/viewingAllvintage/:id" element={<UserVintageDetails />} />
         <Route path="/guest-places" element={<GuestViewVintage/>} />
         <Route path="/tourist-places" element={<TouristViewVintage />} />
+        <Route path="/createVintage" element={<CreateVintageForm />} />
+        <Route path="/sellerProfile" element={<SellerProfile userId={localStorage.getItem("id")} />} />
+
+
         {/* Add more routes as needed */}
       </Routes>
   );
