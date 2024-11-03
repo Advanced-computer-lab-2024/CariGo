99c977b158f94d7a29a1df6f31ad5853d33626f7
mennawahba <melgazar2003@gmail.com>
2024-11-03 18:33:26 +0200

    hotelSearch Update

--- backend/routes/amadusRouter.js
+++ backend/routes/amadusRouter.js
@@ -5,4 +5,6 @@ const router = express.Router();
 router.get('/getFlights',amadusController.getFlights);
 router.get('/cities',amadusController.cities);
 router.get('/hotels',amadusController.getHotels);
+router.get('/hotelDetails',amadusController.getHotelDetails);
+router.get('/seatMap',amadusController.getSeatmap);
 module.exports = router;
